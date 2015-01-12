e83fe86aba6cca9fc8ce78e0fe050d38d2c281f4
WestleyArgentum <WestleyArgentum@gmail.com>
2015-01-11 22:27:33 -0500

    step to end and sort industries by score

--- evaluate-all-industries.jl
+++ evaluate-all-industries.jl
@@ -4,7 +4,10 @@ using InfluenceGame
 
 ateam = create_team(InfluenceGame.game, "A Team")
 
-all_industries = [ id for (id, data) in InfluenceGame.game.industries ]
-add_industries(ateam, all_industries)
+all_industry_codes = [ id for (id, data) in InfluenceGame.game.industries ]
+add_industries(ateam, all_industry_codes)
 
+while step(InfluenceGame.game) end
 
+industries = [ industry for (id, industry) in InfluenceGame.game.industries ]
+sort!(industries; lt = (lhs, rhs) -> lhs["score"] < rhs["score"])
