4463c53f8892bb83f1d14dae8d69b203bf3f30a0
WestleyArgentum <WestleyArgentum@gmail.com>
2015-01-11 22:23:29 -0500

    start creating game-playing scripts

--- src/influence-game.jl
+++ src/influence-game.jl
@@ -1,6 +1,7 @@
 
+module InfluenceGame
 
-#module InfluenceGame
+export create_team, step, add_industry, add_industries, score
 
 using Base.Collections
 using JSON
@@ -8,12 +9,6 @@ using JSON
 include("team.jl")
 include("game.jl")
 
-
 game = Game("./data/112th-bills.json", "./data/112th-industries.json")
 
-ateam = create_team(game, "A Team")
-
-all_industries = [ id for (id, data) in game.industries ]
-add_industries(ateam, all_industries)
-
-#end
+end
